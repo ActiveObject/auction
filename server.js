90744e61442518ec128c54898e45cb47177fb0f2
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-08-07 12:55:18 +0300

    Add basic server implementation

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var app = require('express')();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+  res.send({
+    text: 'Hello, world'
+  });
+});
+
+var server = http.createServer(app);
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
