2d1391266e168bee3443bf1b1afb26acc413c73f
Vasyl Pryimachuk <activeobject.ac@gmail.com>
2015-08-07 14:04:39 +0300

    Enable CORS on the server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var app = require('express')();
+var cors = require('cors');
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send({
